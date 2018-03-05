e15a20605dc82be4681f79635982c6a50ed3ab78
janekjan <jas.piskorski@spoko.pl>
2018-03-05 10:57:00 +0100

    database test

--- index.php
+++ index.php
@@ -6,7 +6,24 @@
 	</head>
 	
 <body>
-	<?php phpinfo(); ?>
+	<?php 
+	$settings = parse_ini_file('db.ini', TRUE);
+	$mysqlUser = $settings['database']['username'];
+	$mysqlPassword = $settings['database']['password'];
+	$mysqlHost = 'mysql:host='.$settings['database']['host'].'dbname='.$settings['database']['schema'];
+	$pdoAttributes = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 
+	'utf8'");
+	
+	try {
+		$pdo = newPDO($mysqlHost, $mysqlUser, $mysqlPassword, 
+		$pdoAttributes);
+		/* Zapytania */
+	} catch (PDOException $e) {
+		echo 'Error: '.$e->getMessage();
+	} finally {
+		$pdo = null;
+	}
+	?>
 </body>
 </html>
 
