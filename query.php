dd6a8adfb5aa623a3e991ab67743b4ac26ee3828
Andrzej <andrzej.fraczak314@gmail.com>
2018-03-11 11:47:44 +0100

    fetch label names directly from db

--- query.php
+++ query.php
@@ -6,13 +6,14 @@
         <h> Choose labels: </h>
 	<form action="queryForm.php" method="post">
 	<?php
-	    $label = array("label1", "label2", "label3");
-	    $numLabels = count($label);
-	    
-	    for($i=0; $i<$numLabels; $i++)
+	    include 'base.php';
+	    $stmt = $pdo->prepare('SHOW COLUMNS FROM min_memes WHERE type="tinyint(1)"');
+	    $stmt->execute();
+	    while($label = $stmt->fetchColumn(0))
 	    {
-		echo "<input type='checkbox' name='checklist[]' value=$label[$i]> $label[$i] <br>";
+		echo "<input type='checkbox' name='checklist[]' value=$label> $label <br>";
 	    }
+	    include 'end.php';
 	?>    
 	<br><input type="submit" value="Show memes">
 	</form>
