648d30002cfce6a4649232f1da96f53845f449f1
janekjan <jas.piskorski@spoko.pl>
2018-04-15 00:48:19 +0200

    Stub of index.php

--- index.php
+++ index.php
@@ -6,7 +6,14 @@
 	<title>Memes AI</title>
 	</head>
 	
-<body>
-</body>
+	<body>
+	  <h1>Memes-AI Temporary Main Page</h1>
+	  [<a >Find Your Favourites</a>][<a href="upload.php">Submit a meme</a>]
+	  <!--
+TODO:
+- "Featured" memes in here
+- better layout and theme
+!-->
+	</body>
 </html>
 <?php include "end.php"; ?>
