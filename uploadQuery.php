a3c19d414c813dcd47a1c28dcb39523720e805ba
janekjan <jas.piskorski@spoko.pl>
2018-05-11 10:11:47 +0200

    I don't know what it does

--- uploadQuery.php
+++ uploadQuery.php
@@ -6,8 +6,17 @@
   <body>
     <form action=" " method="post">
       <?php
-	include 'base.php';
-	$stmt = $pdo->prepare('SHOW COLUMNS FROM min_memes WHERE type="tinyint(1)"');
-        $stmt->execute();
+	  include 'base.php';
+	  $stmt = $pdo->prepare('SHOW COLUMNS FROM min_memes WHERE type="tinyint(1)"');
+	  $stmt->execute();
       while($label = $stmt->fetchColumn(0)) {
+		  echo "<input type='checkbox' name='checklist[]' value=$label> $label <br>";
+	  }
+	  include 'end.php';
+	  ?>
+      <br> <input type="submit" value="Show memes">
+    </form>
+  </body>
+</html>
+  
       
