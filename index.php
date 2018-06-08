9ca350679cc5b2d55b12566ba9fd6e19398ce26d
janekjan <jas.piskorski@spoko.pl>
2018-06-08 10:59:27 +0200

    Gallery style, why featured does not work?

--- index.php
+++ index.php
@@ -9,9 +9,10 @@
 	
 	<body>
 		<?php include "topbar.html"; ?>
+	  <!--
 	  <h1>Memes-AI Temporary Main Page</h1>
 	  [<a href="query.php">Find Your Favourites</a>][<a href="upload.php">Submit a meme</a>]
-	  <!--
+	 
 TODO:
 - "Featured" memes in here
 - better layout and theme
--- queryForm.php
+++ queryForm.php
@@ -16,12 +16,13 @@ else
     //Will this work? If not, review code above
     //UPDATE: It works, but not elegantly
     //$stmt = $pdo->prepare("SELECT id FROM memes JOIN labels ON memes.id=labels.id WHERE $sqlString");
-    $stmt = $pdo->prepare("SELECT id FROM labels WHERE $sqlString");
+    $stmt = $pdo->prepare("SELECT id FROM memes WHERE $sqlString");
     $stmt->execute();
     while($row = $stmt->fetch())
     {
 		//$stmtimg = $pdo->prepare("SELECT id
-		echo('<img src="images/' . $row[0] . '.png" alt="' . $row[0] . '"><br>');
+		echo('<div class="gallery"><a target="_blank" href="images'.$row[0].'.png"><img src="images/'.$row[0].'.png" alt="'.$row[0].'"></a>');
+		echo('<div class="desc">'.$row[2].'</div></div>');
     }
     
 }
